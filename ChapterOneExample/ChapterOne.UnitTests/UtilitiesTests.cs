4844768dd719b6bff4e594460fdb9a319e3f6a59
Augusto Pedraza <augusto.pedraza08@gmail.com>
2013-03-06 13:31:42 -0600

    [Add some test]
       - ShouldFindOneYInMysterious test added to
         ChapterOneExample.UnitTests.UtilitiesTests class.
    
       - ShoudFindTwoSInMysterious test added to
         ChapterOneExample.UnitTests.UtilitiesTests class.
    
       - SearchShouldBeCaseSensitive test added to
         ChapterOneExample.UnitTests.UtilitiesTests class.
    
       - Fix some bad soluction's config.

---
+++ ChapterOneExample/ChapterOne.UnitTests/UtilitiesTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ChapterOneExample.Utilities;
+
+namespace ChapterOneExample.UnitTests
+{
+    public class UtilitiesTests
+    {
+        [Test]
+        public void ShouldFindOneYInMysterious()
+        {
+            var stringToCheck = "mysterious";
+            var stringToFind = "y";
+            var expectedResult = 1;
+            var classUnderTest = new StringUtilities();
+
+            var actualResult =
+                classUnderTest.CountOccurences(stringToCheck, stringToFind);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void ShouldFindTwoSInMysterious()
+        {
+            var stringToCheck = "mysterious";
+            var stringToFind = "s";
+            var expectedResult = 2;
+            var classUnderTest = new StringUtilities();
+
+            var actualResult =
+                classUnderTest.CountOccurences(stringToCheck, stringToFind);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void SearchShouldBeCaseSensitive()
+        {
+            var stringToCheck = "mySterious";
+            var stringToFind = "s";
+            var expectedResult = 2;
+            var classUnderTest = new StringUtilities();
+
+            var actualResult =
+                classUnderTest.CountOccurences(stringToCheck, stringToFind);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ ChapterOneExample/ChapterOneExample.Utilities/StringUtilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChapterOneExample.Utilities
+{
+    public class StringUtilities
+    {
+        public int CountOccurences(string stringToCheck, string stringToFind)
+        {
+            var stringAsCharArray = stringToCheck.ToUpper().ToCharArray();
+            var stringToCheckForAsChar = stringToFind.ToUpper().ToCharArray()[0];
+
+            var occuranceCount = 0;
+
+            for (var characterIndex = 0; characterIndex <= stringAsCharArray.GetUpperBound(0); characterIndex++)
+            {
+                if (stringAsCharArray[characterIndex] == stringToCheckForAsChar)
+                {
+                    occuranceCount++;
+                }
+            }
+
+            return occuranceCount;
+        }
+    }
+}
